772ccdd007f26b46a667afc0ab5e47fdd6fc997a
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-06-14 08:14:23 -0300

    Update Cabeceras.h

--- Cabeceras.h
+++ Cabeceras.h
@@ -1,3 +1,9 @@
+/*
+Van a ver que en algunos programas solo tengo declarado el #include <Cabeceras.h>. 
+Esto es para hcer más cómoda la inclusión de cabeceras sin tener que copiar y pegar cada vez que creo un nuevo archivo .cpp. 
+Entonces solo incluyo la cabecera que tiene todas las cabeceras.
+En el archivo Cabeceras.c tengo todas las cabeceras declaradas que conozco, el using namespace std; y los define que uso normalmente.
+*/
 #include <iostream>
 #include <locale.h>
 #include <stdlib.h>
